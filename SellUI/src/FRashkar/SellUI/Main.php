65e3544f8fe6e855f88895b08e4680c7a0c730ea
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2022-10-25 23:17:15 +0700

    :(

--- SellUI/src/FRashkar/SellUI/Main.php
+++ SellUI/src/FRashkar/SellUI/Main.php
@@ -12,7 +12,7 @@ use pocketmine\plugin\PluginBase;
 
 use FRashkar\SellUI\Commands\SellUICommand;
 
-use Vecnavium\FormsUI\SimpleForm;
+use FRashkar\SellUI\FormsUI\SimpleForm;
 use onebone\economyapi\EconomyAPI;
 
 class Main extends PluginBase implements Listener {
