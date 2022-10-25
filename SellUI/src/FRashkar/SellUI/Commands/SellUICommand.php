2820d67820f229bc836236d93713dfc1107d49d4
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2022-10-25 22:49:13 +0700

    fix :D

--- SellUI/src/FRashkar/SellUI/Commands/SellUICommand.php
+++ SellUI/src/FRashkar/SellUI/Commands/SellUICommand.php
@@ -4,12 +4,11 @@ namespace FRashkar\SellUI\Commands;
 
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
-use pocketmine\event\Listener;
-use pocketmine\plugin\PluginBase;
+
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat;
-use jojoe77777\FormAPI\SimpleForm;
-use onebone\economyapi\EconomyAPI;
+
+use FRashkar/SellUI/Main;
 
 class SellUICommand extends Command {
 
@@ -21,55 +20,9 @@ class SellUICommand extends Command {
     public function execute(CommandSender $sender, string $commandLabel, array $args)
     {
         if($sender instanceof Player) {
-            if($sender->hasPermission("sellui.use")) {
-                    $sender->sendMessage(TextFormat::RED . "Please use this command in-game!");
-        } else {
+            $sender->sendMessage(TextFormat::RED . "Use it in-game please!");
+        }else{
             $this->openSellUI($sender);
-            }
         }
-    }
-    
-    public function openSellUI(Player $player)
-    {
-        $form = new SimpleForm(function(Player $player, int $result = null) {
-            if ($result === null) {
-                return;
-            }
-            // Button 1
-            if ($result === 0) {
-                
-                // Get item in hand
-                $item = $player()->getInventory()->getIntemInHand();
-
-                $price = 0.1; // 1 cobblestone = 0.1 money
-
-                // Total item
-                $total = $item->getCount() * $price;
-
-                // Give money to player
-                EconomyAPI::getInstance()->addMoney($player, $total);
-
-                // Send message to player
-                $player->sendMessage("You have recieved " . $total . "for selling Cobblestone x" . $item->getCount());
-
-                // Reset item
-                $player->getInventory()->remove($item);
-                
-            }});
-
-        $item = $player()->getInventory()->getIntemInHand();
-
-        if ($item->getId() != 4) {
-            $player->sendMessage("You can only sell cobblestone!");
-            return;
-        }
-
-        // Form
-        $form->setTitle( title: "Sell UI");
-        $form->setContent( content: "Do you want to sell cobblestone x" . $item->getCount() . " ?");
-        $form->addButton( text: "Sell now!");
-
-        // Send form to player
-        $player->sendForm($form);
     }
  }
