d5404e4f1ff06106f433c57f17923e91c680aa64
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2022-10-26 17:03:00 +0700

    sry mistake, ok

--- SellUI/src/FRashkar/SellUI/Commands/SellUICommand.php
+++ SellUI/src/FRashkar/SellUI/Commands/SellUICommand.php
@@ -28,7 +28,7 @@ class SellUICommand extends Command {
             $sender->sendMessage(TextFormat::RED . "Please use it in-game!");
         }else{
             $this->getOwningPlugin()->openSellUI($sender);
-            $this->sendMessage(TextFormat::GREEN . "Open SellUI");
+            $sender->sendMessage(TextFormat::GREEN . "Open SellUI");
         }
         return true;
     }
